d5d3dc7ac97cd4aa635cf9481078a74a2ca38f9c
Ilham D. Sofyan <ilhamdsofyan@gmail.com>
2021-04-19 05:52:32 +0700

    Hapus fitur yg tidak perlu ada di core irbac

--- application/modules/cuti/views/verifikasi/_partial/css.php
+++
@@ -1,3 +0,0 @@
-<link href="<?= base_url('/web/assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/datatables/datatables.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') ?>" rel="stylesheet" type="text/css" />
--- application/views/live-attendance/_partial/sub_css.php
+++
@@ -1,6 +0,0 @@
-<link href="<?= base_url('/web/assets/global/plugins/datatables/datatables.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/bootstrap-select2/css/select2.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/bootstrap-select2/css/select2-bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('web/assets/pages/css/profile.min.css') ?>" rel="stylesheet" type="text/css" />
