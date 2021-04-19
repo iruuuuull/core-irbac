695725e547677b99c13fa97391671cff317cbb38
Ilham D. Sofyan <ilhamdsofyan@gmail.com>
2021-04-20 04:52:01 +0700

    Penyesuaian style halaman menu

--- application/modules/rbac/views/menu/index.php
+++ application/modules/rbac/views/menu/index.php
@@ -2,8 +2,8 @@
 
 <div class="row">
     <div class="col-md-12">
-        <div class="portlet light bordered">
-            <div class="portlet-body">
+        <div class="card light bordered">
+            <div class="card-body">
                 <div class="row">
                     <div class="col-md-4">
                         <?= form_open(); ?>
