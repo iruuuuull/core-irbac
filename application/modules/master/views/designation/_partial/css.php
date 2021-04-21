e16dc18ded25d3c9b70e15df8258d5c48508e232
Ilham D. Sofyan <ilhamdsofyan@gmail.com>
2021-04-21 11:48:59 +0700

    Fix tampilan designation

--- application/modules/master/views/designation/_partial/css.php
+++ application/modules/master/views/designation/_partial/css.php
@@ -1,4 +1,1 @@
-<link href="<?= base_url('/web/assets/global/plugins/datatables/datatables.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/select2/css/select2.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('/web/assets/global/plugins/select2/css/select2-bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?= base_url('/web/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>" rel="stylesheet" type="text/css" />
